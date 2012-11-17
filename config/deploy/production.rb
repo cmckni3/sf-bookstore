8354b97157edeed79f59c1e801e6caef73b6c2ed
Chris McKnight <cmckni3@gmail.com>
2012-11-17 02:34:27 -0600

    Added capistrano

---
+++ config/deploy/production.rb
@@ -0,0 +1,8 @@
+server '192.168.33.12', :app, :web, :db, primary: true
+set :keep_releases, 5
+set :stage_domain_name, "production"
+set :branch, "master"
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+end
